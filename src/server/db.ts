abaffe05e878bfa8c3c85767e7c235d9a15c1bfd
Andresjl389 <andresjaimes195@gmail.com>
2026-07-12 20:32:04 -0500

    feat: [TASK-18] set up Prisma with PostgreSQL and initial database schema

---
+++ src/server/db.ts
@@ -0,0 +1,17 @@
+import { PrismaClient } from "@/generated/prisma/client";
+import { Pool } from "pg";
+import { PrismaPg } from "@prisma/adapter-pg";
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+});
+
+const adapter = new PrismaPg(pool);
+
+export const db = new PrismaClient({
+  adapter,
+  log:
+    process.env.NODE_ENV === "development"
+      ? ["query", "error", "warn"]
+      : ["error"],
+});
